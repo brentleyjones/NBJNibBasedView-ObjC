868349f0a6a92632f56714ba1ff08225131bc2ca
Brentley Jones <Brentley.Jones@target.com>
2014-06-23 09:24:13 -0500

    Fix comments

--- NBJNibBasedView/NBJNibBasedView.h
+++ NBJNibBasedView/NBJNibBasedView.h
@@ -9,21 +9,21 @@
 @import Foundation;
 
 /**
- *  A nib-based view will load it's view from a specified nib
+ *  A nib-based view will load it's view from a specified nib
  *  (defaults to class name).
- *  This allows for custom views and controls to be designed in a nib and
+ *  This allows for custom views and controls to be designed in a nib and
  *  included in other storyboards and nibs without convoluted steps.
  *  To add this functionality to a view, simply add the `NBJNibBasedView`
  *  protocol to the view.
  *
- *  This works because the catagory `UIView (`NBJNibBasedView)` uses method
+ *  This works because the catagory `UIView (NBJNibBasedView)` uses method
  *  swizzling to swizzle <initWithFrame:>, in which it checks for conformace
  *  to the `NBJNibBasedView` protocol. If the view conforms to this protocol it
  *  does its magic.
  *
- *  Using a protocol and method swizzling is prefered because it allows the base
- *  class to be whatever it needs to be. It can for example be a <UIView> or
- *  a <UIControl>.
+ *  Using a protocol and method swizzling is prefered because it allows the base
+ *  class to be whatever it needs to be. It can for example be a <UIView> or
+ *  a <UIControl>.
  */
 @protocol NBJNibBasedView <NSObject>
 
